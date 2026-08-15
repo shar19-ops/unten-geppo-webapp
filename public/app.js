33bf6b2838d2c65bdb941dbdce1771aaa012d680
shar19-ops <shar19@mac.com>
2026-08-15 11:00:54 +0900

    管理者パスワードを変更する
    履歴に残っていた旧パスワードは公開情報同然のため無効化。新しい値は
    リポジトリがpublicであることを踏まえコミットメッセージ・DEVLOGに記載しない。

--- public/app.js
+++ public/app.js
@@ -169,7 +169,7 @@ document.querySelectorAll('.tab-btn').forEach((btn) => {
 });
 
 // ---------------- 管理者パスワード保護(車両リスト) ----------------
-const ADMIN_PASSWORD = 'anzn';
+const ADMIN_PASSWORD = 'untn';
 const ADMIN_UNLOCK_KEY = 'ug_admin_unlocked';
 
 function isAdminUnlocked() {
